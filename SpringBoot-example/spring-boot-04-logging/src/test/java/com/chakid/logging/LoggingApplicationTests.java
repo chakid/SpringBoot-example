212ee3a5f46681c394cfd13205f78dae7463249c
Chakid <chakid125@gmail.com>
2019-06-19 00:29:02 +0800

    add logging

--- SpringBoot-example/spring-boot-04-logging/src/test/java/com/chakid/logging/LoggingApplicationTests.java
+++ SpringBoot-example/spring-boot-04-logging/src/test/java/com/chakid/logging/LoggingApplicationTests.java
@@ -11,10 +11,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class LoggingApplicationTests {
 
+    //日志声明
     Logger logger = LoggerFactory.getLogger(getClass());
 
     @Test
     public void contextLoads() {
+
+
+        //日志级别 级别由低到高 trace<debug<info<warn<error
+        logger.trace("这是trace跟踪信息");
+
+        logger.debug("这是debug调试信息");
+
+        logger.info("这是info日志");
+
+        logger.warn("这是warn警告信息");
+
+        logger.error("这是error错误信息");
 
     }
 
